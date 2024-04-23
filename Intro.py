6bea198556f0687e7ac2e91d5096cc1ad438066b
NicolasRP30 <166547526+NicolasRP30@users.noreply.github.com>
2024-04-23 13:53:06 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -3,9 +3,6 @@ import pandas as pd
 import numpy as np
 import geopandas as gpd
 import json
-
-with open('total_incidentes_transito.geojson', "r") as read_file:
-    data = json.load(read_file)
   
 st.title("Esta es la intro")
 
