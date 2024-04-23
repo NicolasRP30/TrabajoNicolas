889dabee2abbdaa06c8dcfecf565ad764cc8fe2c
NicolasRP30 <166547526+NicolasRP30@users.noreply.github.com>
2024-04-23 14:18:38 -0500

    Update AI.py

--- pages/AI.py
+++ pages/AI.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import json
+
+from PIL import Image
 
 st.title("Esto es Inteligencia Artificial")
+
+st.write("Aqui podemos ver el proyecto de Inteligencia Artificial desarrollado en la materia de Inteligencia Urbana")
+
+image = Image.open('DetectorIma.jpg')
+st.image(image)
