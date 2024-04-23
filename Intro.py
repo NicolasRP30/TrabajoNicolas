636f203db15e599af691356995cfaa338b709cc9
NicolasRP30 <166547526+NicolasRP30@users.noreply.github.com>
2024-04-23 13:51:15 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import json
 
+with open('total_incidentes_transito.geojson', "r") as read_file:
+    data = json.load(read_file)
+  
 st.title("Esta es la intro")
+
+
+st.write("Esta es la recopilación de trabajos que se han realizado en la materia de Inteligencia Urbana")
+
