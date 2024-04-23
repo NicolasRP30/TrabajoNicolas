2db32dba8fea6dae1cfb9fb596f9672c234e4a7b
NicolasRP30 <166547526+NicolasRP30@users.noreply.github.com>
2024-04-23 14:09:01 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -4,8 +4,12 @@ import numpy as np
 import geopandas as gpd
 import json
   
-st.title("Esta es la intro")
-
+st.title("Introduccion de Inteligencia Urbana")
 
 st.write("Esta es la recopilación de trabajos que se han realizado en la materia de Inteligencia Urbana")
+
+from PIL import Image
+
+image = Image.open('Ciudade_Inteligentes.jpeg')
+st.image(image)
 
