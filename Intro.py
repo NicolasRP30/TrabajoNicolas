970dfe78931bef44a4536803643f4bd76c474b8d
NicolasRP30 <166547526+NicolasRP30@users.noreply.github.com>
2024-04-30 14:04:52 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -10,7 +10,8 @@ st.title("Esta es la intro a la Inteligencia Urbana")
 
 st.write("Esta es la recopilación de trabajos que se han realizado en la materia de Inteligencia Urbana en el semestre 2024 - 1" 
 "en compañia con el profesor Carlos Mario. Este semestre hemos profundizado en conceptos como IoT o tambien conocido como" 
-"Internet de las Cosas, Analitica de datos y la famosa AI con sus herramientas.")
+"Internet de las Cosas, Analitica de datos y la famosa AI con sus herramientas. Para el diseño urbano estas son herrmaientas" 
+"poderosas para el dar soluciones a retos urbanos y rurales que mejoren la calidad de vida de los habitantes")
 
 image = Image.open('Ciudades_Inteligentes.jpg')
 st.image(image)
